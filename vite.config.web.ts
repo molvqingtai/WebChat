129129c455966268a2c9360cb343bb4a8032ce82
molvqingtai <molvqingtai@gmail.com>
2023-07-31 00:36:44 +0800

    chore: delete unplugin-icons

--- vite.config.web.ts
+++ vite.config.web.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Icons from 'unplugin-icons/vite'
+
 import packageJson from './package.json'
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -16,9 +16,5 @@ export default defineConfig({
     __DEV__: isDev,
     __NAME__: JSON.stringify(packageJson.name)
   },
-  plugins: [
-    react(),
-    // https://github.com/antfu/unplugin-icons
-    Icons({ compiler: 'jsx', jsx: 'react' })
-  ]
+  plugins: [react()]
 })
